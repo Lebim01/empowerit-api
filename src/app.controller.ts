7014b766d044504a3f3c6fc1a0b6a61999b4ca7f
victor alvarez <victoralvarezsaucedo@gmail.com>
2023-08-03 15:19:22 -0600

    create link

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Get, Headers, Post } from "@nestjs/common";
 import { AppService } from "./app.service";
 import axios from "axios";
 
@@ -7,14 +7,27 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get("cryptoapisverifydomain")
-  verifyDomain(){
+  verifyDomain() {
     return "cryptoapis-cb-3c5ed9409121d6814c3c7383372faefb3ed72ccc4775a42c56c49e92949fc616";
   }
 
   @Post("callbackPayment")
-  async callbackPayment(@Body() body): Promise<any> {
+  async callbackPayment(@Body() body, @Headers() headers): Promise<any> {
     return axios.post(
       "https://onconfirmedtransaction-mdx7upthia-uc.a.run.app",
+      body,
+      {
+        headers: {
+          "x-signature": headers["x-signature"],
+        },
+      }
+    );
+  }
+
+  @Post("createPaymentAddress")
+  async create(@Body() body) {
+    return axios.post(
+      "https://createpaymentaddress-mdx7upthia-uc.a.run.app",
       body
     );
   }
