59d36ab5f4cb9b2c933fd8ca45596be28d5a222e
victor alvarez <victoralvarezsaucedo@gmail.com>
2023-08-03 11:47:59 -0600

    verify domain

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,15 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Get, Post } from "@nestjs/common";
 import { AppService } from "./app.service";
 import axios from "axios";
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get("cryptoapisverifydomain")
+  verifyDomain(){
+    return "cryptoapis-cb-3c5ed9409121d6814c3c7383372faefb3ed72ccc4775a42c56c49e92949fc616";
+  }
 
   @Post("callbackPayment")
   async callbackPayment(@Body() body): Promise<any> {
