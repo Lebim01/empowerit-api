8e461cd7c46c165d7eb128915dc92d870c9d0863
Victor Alvarez <victoralvarezsaucedo@gmail.com>
2023-08-23 21:15:10 -0600

    add sentry

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as Sentry from '@sentry/node';
+import { SentryFilter } from './sentry/sentry.filter';
 
 async function bootstrap() {
+  Sentry.init({
+    dsn: process.env.SENTRY_DNS,
+    environment: process.env.SENTRY_ENV,
+    enabled: true,
+    debug: true,
+  });
+
   const app = await NestFactory.create(AppModule, {
     cors: true,
   });
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new SentryFilter(httpAdapter));
 
   await app.listen(8080);
 }
