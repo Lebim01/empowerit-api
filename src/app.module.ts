d0230296f0efd4c475d499735247a11dbaaeffdc
Victor Alvarez <victoralvarezsaucedo@gmail.com>
2023-08-23 20:09:07 -0600

    sentry

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,8 +14,16 @@ import { BinaryModule } from './binary/binary.module';
 
 import { RanksModule } from './ranks/ranks.module';
 import { RanksService } from './ranks/ranks.service';
+import { SentryModule } from '@ntegral/nestjs-sentry';
+
 @Module({
   imports: [
+    SentryModule.forRoot({
+      dsn: 'https://84117b0558246fc197f3ffdb42db404e@o4505757456334848.ingest.sentry.io/4505757458366464',
+      debug: process.env.SENTRY_ENV && process.env.SENTRY_ENV != 'local',
+      environment: process.env.SENTRY_ENV,
+      logLevels: ['debug'],
+    }),
     CryptoapisModule,
     BondsModule,
     SubscriptionsModule,
