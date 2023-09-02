7edae32faf95f4b908e3162416d822e1c018644b
Victor Alvarez <victoralvarezsaucedo@gmail.com>
2023-09-02 00:25:25 -0600

    revert payroll

--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -83,7 +83,7 @@ export class AdminService {
   async payroll() {
     const payroll_data = await this.getPayroll();
 
-    /*const ref = await db.collection('payroll').add({
+    const ref = await db.collection('payroll').add({
       total_usd: payroll_data.reduce((a, b) => a + b.total, 0),
       total_btc: payroll_data.reduce((a, b) => a + b.btc_amount, 0),
       created_at: new Date(),
@@ -96,7 +96,7 @@ export class AdminService {
           created_at: new Date(),
         });
       }),
-    );*/
+    );
 
     for (const doc of payroll_data) {
       await db.doc('users/' + doc.id).update({
