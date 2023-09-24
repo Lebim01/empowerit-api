12032c4d66fc1d56a8702971bfc94fef522447d7
Victor Alvarez <victoralvarezsaucedo@gmail.com>
2023-09-24 13:26:42 -0600

    fix timezone and current week

--- src/ranks/ranks.service.ts
+++ src/ranks/ranks.service.ts
@@ -365,7 +365,10 @@ export class RanksService {
   }
 
   async getWeeks() {
-    const sunday_this_week = dayjs().startOf('week').hour(23);
+    const sunday_this_week = dayjs()
+      .subtract(dayjs().day() == 6 ? 7 : 0, 'day')
+      .startOf('week')
+      .hour(23);
     const sunday_2_weeks = sunday_this_week.subtract(1, 'week');
     const sunday_3_weeks = sunday_this_week.subtract(2, 'week');
     const sunday_4_weeks = sunday_this_week.subtract(3, 'week');
@@ -376,6 +379,13 @@ export class RanksService {
       [sunday_2_weeks, sunday_2_weeks.add(7, 'days')],
       [sunday_this_week, sunday_this_week.add(7, 'days')],
     ];
+
+    console.log(
+      dates.map(([start, end]) => ({
+        start: start.format('YYYY-MM-DD HH:mm:ss'),
+        end: end.format('YYYY-MM-DD HH:mm:ss'),
+      })),
+    );
 
     return dates;
   }
