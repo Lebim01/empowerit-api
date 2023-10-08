2eca713b97fcaf840da189f2d99025b1aed57fa4
Victor Alvarez <victoralvarezsaucedo@gmail.com>
2023-10-08 11:17:54 -0600

    cloudflare folder

--- src/cloudflare/cloudflare.controller.ts
+++ src/cloudflare/cloudflare.controller.ts
@@ -7,6 +7,6 @@ export class CloudflareController {
 
   @Post('uploadVideo')
   uploadVideo(@Body() body) {
-    return this.cloudflareService.getUploadVideoUrl(body.courseId, 'lives');
+    return this.cloudflareService.getUploadVideoUrl(body.courseId, body.folder);
   }
 }
