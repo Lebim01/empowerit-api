66153479bb97a73a24a075c8df2259841804adbb
victor alvarez <victoralvarezsaucedo@gmail.com>
2023-11-25 12:28:46 -0600

    fix payroll

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,54 +1,58 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
-import { AdminService } from './admin.service';
-import { CallbackTransaction } from 'src/cryptoapis/types';
+import { Body, Controller, Get, Param, Post, Query } from "@nestjs/common";
+import { AdminService } from "./admin.service";
+import { CallbackTransaction } from "src/cryptoapis/types";
 
-@Controller('admin')
+@Controller("admin")
 export class AdminController {
   constructor(private readonly adminService: AdminService) {}
 
-  @Get('/payroll')
-  getpayroll(@Query('blockchain') blockchain: 'bitcoin' | 'xrp') {
+  @Get("/payroll")
+  getpayroll(@Query("blockchain") blockchain: "bitcoin" | "xrp") {
     return this.adminService.getPayroll(blockchain);
   }
 
-  @Post('/payroll')
-  payroll(@Query('blockchain') blockchain: 'bitcoin' | 'xrp' = 'xrp') {
+  @Post("/payroll")
+  payroll(@Query("blockchain") blockchain: "bitcoin" | "xrp" = "xrp") {
     return this.adminService.payroll(blockchain);
   }
 
-  @Post('/payroll/:payrollid')
+  @Post("/payroll/:payrollid")
   payrollFromPayroll(
-    @Param('payrollid') id: string,
-    @Query('blockchain') blockchain: 'xrp' | 'bitcoin',
+    @Param("payrollid") id: string,
+    @Query("blockchain") blockchain: "xrp" | "bitcoin"
   ) {
     return this.adminService.payrollFromPayroll(id, blockchain);
   }
 
-  @Get('/payroll/:payrollid/amount')
-  payrollAmount(@Param('payrollid') id: string) {
+  @Get("/payroll/:payrollid/amount")
+  payrollAmount(@Param("payrollid") id: string) {
     return this.adminService.fixPayrollAmount(id);
   }
 
-  @Post('/withdraw')
+  @Post("/withdraw")
   withdraw(@Body() body) {
     return this.adminService.withdraw(
       body.address,
       body.amount,
-      body.blockchain,
+      body.blockchain
     );
   }
 
-  @Post('/callbackSendedCoins/xrp/:address/:amount')
+  @Post("/callbackSendedCoins/xrp/:address/:amount")
   confirmPayment(
-    @Param('address') address: string,
-    @Param('amount') amount: string,
-    @Body() body: CallbackTransaction,
+    @Param("address") address: string,
+    @Param("amount") amount: string,
+    @Body() body: CallbackTransaction
   ) {
-    console.log(body);
-    if (body.data.event != 'TRANSACTION_REQUEST_REJECTION') {
+    if (body.data.event != "TRANSACTION_REQUEST_REJECTION") {
       return this.adminService.reduceWalletAmount(address, Number(amount));
     } else {
-      return 'OK';
+      return "OK";
     }
+  }
+
+  @Post("/lacktopay/:payroll")
+  confirmPayment(@Param("payroll") payrollID: string) {
+    return this.adminService.fixPayLack(payrollID);
   }
 }
--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,11 +1,11 @@
-import { Injectable } from '@nestjs/common';
-import { db } from '../firebase/admin';
-import { CryptoapisService } from '../cryptoapis/cryptoapis.service';
-import { ranks_object } from '../ranks/ranks_object';
-import { BinaryService } from '../binary/binary.service';
-import dayjs from 'dayjs';
-import { firestore } from 'firebase-admin';
-import { Encode } from 'xrpl-tagged-address-codec';
+import { Injectable } from "@nestjs/common";
+import { db } from "../firebase/admin";
+import { CryptoapisService } from "../cryptoapis/cryptoapis.service";
+import { ranks_object } from "../ranks/ranks_object";
+import { BinaryService } from "../binary/binary.service";
+import dayjs from "dayjs";
+import { firestore } from "firebase-admin";
+import { Encode } from "xrpl-tagged-address-codec";
 
 const ADMIN_BINARY_PERCENT = 17 / 100;
 
@@ -15,25 +15,25 @@ const MINIMAL_XRP_AMOUNT = 20;
 export class AdminService {
   constructor(
     private readonly cryptoapisService: CryptoapisService,
-    private readonly binaryService: BinaryService,
+    private readonly binaryService: BinaryService
   ) {}
 
-  async getPayroll(blockchain: 'bitcoin' | 'xrp') {
-    const users = await db.collection('users').get();
+  async getPayroll(blockchain: "bitcoin" | "xrp") {
+    const users = await db.collection("users").get();
     const docs = users.docs.map((r) => ({ id: r.id, ...r.data() }));
 
     const payroll_data = docs
       .filter((docData: any) => {
         // los STARTER no cobran
         const is_starter =
-          docData.subscription?.starter?.status == 'paid' || false;
+          docData.subscription?.starter?.status == "paid" || false;
         return !is_starter;
       })
       .map((docData: any) => {
         const isAdmin =
-          Boolean(docData.is_admin) || docData.type == 'top-lider';
+          Boolean(docData.is_admin) || docData.type == "top-lider";
         const binary_side =
-          docData.left_points > docData.right_points ? 'right' : 'left';
+          docData.left_points > docData.right_points ? "right" : "left";
         const rank = ranks_object[docData.rank];
         const binary_points =
           rank.binary > 0 || isAdmin ? docData[`${binary_side}_points`] : 0;
@@ -56,7 +56,7 @@ export class AdminService {
             Math.floor(
               binary_points *
                 (isAdmin ? ADMIN_BINARY_PERCENT : rank.binary) *
-                100,
+                100
             ) / 100,
           binary_percent: isAdmin ? ADMIN_BINARY_PERCENT : rank.binary,
           binary_side,
@@ -102,28 +102,28 @@ export class AdminService {
       payroll_data.map(async (doc) => ({
         ...doc,
         crypto_amount:
-          blockchain == 'bitcoin'
+          blockchain == "bitcoin"
             ? await this.cryptoapisService.getBTCExchange(doc.total)
-            : blockchain == 'xrp'
+            : blockchain == "xrp"
             ? await this.cryptoapisService.getXRPExchange(doc.total)
             : 0,
-      })),
+      }))
     );
 
     return payroll_data_2;
   }
 
-  async payroll(blockchain: 'bitcoin' | 'xrp') {
+  async payroll(blockchain: "bitcoin" | "xrp") {
     const payroll_data = await this.getPayroll(blockchain);
 
-    const ref = await db.collection('payroll').add({
+    const ref = await db.collection("payroll").add({
       total_usd: payroll_data.reduce((a, b) => a + b.total, 0),
       total_btc: payroll_data.reduce((a, b) => a + b.crypto_amount, 0),
       created_at: new Date(),
     });
     await Promise.all(
       payroll_data.map(async (doc) => {
-        await ref.collection('details').add(doc);
+        await ref.collection("details").add(doc);
         await db.collection(`users/${doc.id}/payroll`).add({
           ...doc,
           created_at: new Date(),
@@ -131,11 +131,11 @@ export class AdminService {
         if (doc.binary_points > 0) {
           await this.binaryService.matchBinaryPoints(doc.id);
         }
-      }),
+      })
     );
 
     for (const doc of payroll_data) {
-      await db.doc('users/' + doc.id).update({
+      await db.doc("users/" + doc.id).update({
         profits: doc.profits + doc.total,
         bond_direct: 0,
         bond_direct_second_level: 0,
@@ -152,14 +152,14 @@ export class AdminService {
       });
     }
 
-    if (blockchain == 'bitcoin') {
+    if (blockchain == "bitcoin") {
       await this.cryptoapisService.sendRequestTransaction(
         payroll_data.map((doc) => ({
           address: doc.wallet_bitcoin,
           amount: `${doc.crypto_amount}`,
-        })),
+        }))
       );
-    } else if (blockchain == 'xrp') {
+    } else if (blockchain == "xrp") {
       const wallets = await Promise.all(
         payroll_data.map(async (doc) => {
           const xAddress = await this.getXAddressUser(doc.id);
@@ -168,7 +168,7 @@ export class AdminService {
             amount: doc.crypto_amount,
             id_user: doc.id,
           };
-        }),
+        })
       );
       await this.payrollWithXRP(ref.id, wallets);
     }
@@ -182,7 +182,7 @@ export class AdminService {
       id_user: string;
       xAddress: string;
       amount: number;
-    }[],
+    }[]
   ) {
     /**
      * Total a liquidar descontando la cantidad ya confirmada
@@ -190,13 +190,13 @@ export class AdminService {
     let total = 0;
     for (const user of payroll_users) {
       const transactionRef = await db
-        .collection('payroll')
+        .collection("payroll")
         .doc(payroll_id)
-        .collection('transactions')
+        .collection("transactions")
         .doc(user.id_user)
         .get();
 
-      user.amount -= transactionRef.get('confirmed_amount') || 0;
+      user.amount -= transactionRef.get("confirmed_amount") || 0;
       total += user.amount;
     }
 
@@ -249,9 +249,9 @@ export class AdminService {
     if (payroll_id != null) {
       for (const doc of wallets_users) {
         const transactionRef = await db
-          .collection('payroll')
+          .collection("payroll")
           .doc(payroll_id)
-          .collection('transactions')
+          .collection("transactions")
           .doc(doc.id_user)
           .get();
 
@@ -276,7 +276,7 @@ export class AdminService {
           await this.cryptoapisService.sendXRPTransactionFromAddress(
             wu.xAddress,
             address.address,
-            address.amount_to_transfer.toFixed(6),
+            address.amount_to_transfer.toFixed(6)
           );
         } catch (err) {
           console.error(err);
@@ -290,16 +290,16 @@ export class AdminService {
    */
   async getXRPWalletsToUse(total: number) {
     const wallets = await db
-      .collection('wallets')
-      .where('currency', '==', 'XRP')
-      .where('amount', '>', 20.1)
+      .collection("wallets")
+      .where("currency", "==", "XRP")
+      .where("amount", ">", 20.1)
       .get();
 
     let amount = 0;
     const wallets_to_use = [];
 
     for (const wallet of wallets.docs) {
-      amount += wallet.get('amount') - MINIMAL_XRP_AMOUNT;
+      amount += wallet.get("amount") - MINIMAL_XRP_AMOUNT;
       wallets_to_use.push(wallet.data());
 
       if (amount >= total) break;
@@ -311,27 +311,27 @@ export class AdminService {
   /**
    * Enviar transacción a cryptoapis usando un registro de payroll
    */
-  async payrollFromPayroll(id: string, blockchain: 'bitcoin' | 'xrp') {
+  async payrollFromPayroll(id: string, blockchain: "bitcoin" | "xrp") {
     const payroll_data = await db
-      .collection('payroll')
+      .collection("payroll")
       .doc(id)
-      .collection('details')
+      .collection("details")
       .get();
 
-    if (blockchain == 'bitcoin') {
+    if (blockchain == "bitcoin") {
       const res = await this.cryptoapisService.sendRequestTransaction(
         payroll_data.docs.map((doc) => ({
-          address: doc.get('wallet_bitcoin'),
-          amount: `${doc.get('crypto_amount')}`,
-        })),
+          address: doc.get("wallet_bitcoin"),
+          amount: `${doc.get("crypto_amount")}`,
+        }))
       );
       return res;
-    } else if (blockchain == 'xrp') {
+    } else if (blockchain == "xrp") {
       const wallets = await Promise.all(
         payroll_data.docs.map(async (doc) => {
-          const id_user = doc.get('id');
+          const id_user = doc.get("id");
           const total_xrp = await this.cryptoapisService.getXRPExchange(
-            doc.get('total'),
+            doc.get("total")
           );
           const xAddress = await this.getXAddressUser(id_user);
           return {
@@ -339,7 +339,7 @@ export class AdminService {
             xAddress,
             amount: total_xrp,
           };
-        }),
+        })
       );
       await this.payrollWithXRP(id, wallets);
 
@@ -353,14 +353,14 @@ export class AdminService {
    */
   async fixPayrollAmount(id: string) {
     const payroll_data = await db
-      .collection('payroll')
+      .collection("payroll")
       .doc(id)
-      .collection('details')
+      .collection("details")
       .get();
 
     const amount = payroll_data.docs.reduce(
-      (a, b) => a + Number(b.get('total')),
-      0,
+      (a, b) => a + Number(b.get("total")),
+      0
     );
 
     return amount;
@@ -369,21 +369,21 @@ export class AdminService {
   async withdraw(
     address: string,
     amount_usd: string,
-    blockchain: 'xrp' | 'bitcoin',
+    blockchain: "xrp" | "bitcoin"
   ) {
-    if (blockchain == 'bitcoin')
+    if (blockchain == "bitcoin")
       return this.cryptoapisService.withdraw(address, amount_usd);
-    else if (blockchain == 'xrp') {
+    else if (blockchain == "xrp") {
       const amount_xrp = await this.cryptoapisService.getXRPExchange(
-        Number(amount_usd),
+        Number(amount_usd)
       );
       const xAddress = await this.getXAddressUser(
-        'JpdntP2OQzNSBi3IylyMfSEqqSD2',
+        "JpdntP2OQzNSBi3IylyMfSEqqSD2"
       );
       return this.payrollWithXRP(null, [
         {
           amount: amount_xrp,
-          id_user: 'JpdntP2OQzNSBi3IylyMfSEqqSD2',
+          id_user: "JpdntP2OQzNSBi3IylyMfSEqqSD2",
           xAddress,
         },
       ]);
@@ -392,24 +392,24 @@ export class AdminService {
 
   async regresardinero() {
     const snap = await db
-      .collectionGroup('payroll')
-      .where('created_at', '>=', dayjs('2023-10-29 00:00:00').toDate())
+      .collectionGroup("payroll")
+      .where("created_at", ">=", dayjs("2023-10-29 00:00:00").toDate())
       .get();
 
     for (const doc of snap.docs) {
-      if (doc.ref.path.includes('users')) {
+      if (doc.ref.path.includes("users")) {
         const get_bonds = (doc) => ({
-          bond_direct: doc.get('direct'),
-          bond_direct_second_level: doc.get('direct_second_level'),
-          bond_residual_level_1: doc.get('residual'),
-          bond_residual_level_2: doc.get('residual_second_level'),
-          bond_scholarship_level_1: doc.get('scholarship'),
-          bond_scholarship_level_2: doc.get('scholarship_second_level'),
-          bond_scholarship_level_3: doc.get('scholarship_third_level'),
-          bond_supreme_level_1: doc.get('supreme'),
-          bond_supreme_level_2: doc.get('supreme_second_level'),
-          bond_supreme_level_3: doc.get('supreme_third_level'),
-          bond_direct_starter_level_1: doc.get('bond_direct_starter_level_1'),
+          bond_direct: doc.get("direct"),
+          bond_direct_second_level: doc.get("direct_second_level"),
+          bond_residual_level_1: doc.get("residual"),
+          bond_residual_level_2: doc.get("residual_second_level"),
+          bond_scholarship_level_1: doc.get("scholarship"),
+          bond_scholarship_level_2: doc.get("scholarship_second_level"),
+          bond_scholarship_level_3: doc.get("scholarship_third_level"),
+          bond_supreme_level_1: doc.get("supreme"),
+          bond_supreme_level_2: doc.get("supreme_second_level"),
+          bond_supreme_level_3: doc.get("supreme_third_level"),
+          bond_direct_starter_level_1: doc.get("bond_direct_starter_level_1"),
         });
 
         const bonds = get_bonds(doc);
@@ -417,34 +417,34 @@ export class AdminService {
         await doc.ref.parent.parent.update({
           bond_direct: firestore.FieldValue.increment(bonds.bond_direct),
           bond_direct_second_level: firestore.FieldValue.increment(
-            bonds.bond_direct_second_level,
+            bonds.bond_direct_second_level
           ),
           bond_residual_level_1: firestore.FieldValue.increment(
-            bonds.bond_residual_level_1,
+            bonds.bond_residual_level_1
           ),
           bond_residual_level_2: firestore.FieldValue.increment(
-            bonds.bond_residual_level_2,
+            bonds.bond_residual_level_2
           ),
           bond_scholarship_level_1: firestore.FieldValue.increment(
-            bonds.bond_scholarship_level_1,
+            bonds.bond_scholarship_level_1
           ),
           bond_scholarship_level_2: firestore.FieldValue.increment(
-            bonds.bond_scholarship_level_2,
+            bonds.bond_scholarship_level_2
           ),
           bond_scholarship_level_3: firestore.FieldValue.increment(
-            bonds.bond_scholarship_level_3,
+            bonds.bond_scholarship_level_3
           ),
           bond_supreme_level_1: firestore.FieldValue.increment(
-            bonds.bond_supreme_level_1,
+            bonds.bond_supreme_level_1
           ),
           bond_supreme_level_2: firestore.FieldValue.increment(
-            bonds.bond_supreme_level_2,
+            bonds.bond_supreme_level_2
           ),
           bond_supreme_level_3: firestore.FieldValue.increment(
-            bonds.bond_supreme_level_3,
+            bonds.bond_supreme_level_3
           ),
           bond_direct_starter_level_1: firestore.FieldValue.increment(
-            bonds.bond_direct_starter_level_1,
+            bonds.bond_direct_starter_level_1
           ),
         });
 
@@ -455,36 +455,36 @@ export class AdminService {
 
   async reduceWalletAmount(address: string, amount: number) {
     const addresses = await db
-      .collection('wallets')
-      .where('address', '==', address)
+      .collection("wallets")
+      .where("address", "==", address)
       .get();
 
     if (addresses.size > 0) {
-      await addresses.docs[0].ref.collection('history').add({
+      await addresses.docs[0].ref.collection("history").add({
         before: addresses.docs[0].data(),
         payload: {
-          description: 'Withdraw',
+          description: "Withdraw",
           amount,
           created_at: new Date(),
         },
       });
       await addresses.docs[0].ref.update({
-        amount: addresses.docs[0].get('amount') - amount,
+        amount: addresses.docs[0].get("amount") - amount,
         updated_at: new Date(),
       });
     }
   }
 
   async getXAddressUser(id_user: string) {
-    const user = await db.collection('users').doc(id_user).get();
-    const address = user.get('wallet_ripple');
-    const tag = user.get('wallet_ripple_tag');
+    const user = await db.collection("users").doc(id_user).get();
+    const address = user.get("wallet_ripple");
+    const tag = user.get("wallet_ripple_tag");
 
     if (!address) {
-      throw new Error('No wallet');
+      throw new Error("No wallet");
     }
 
-    let xAddress = user.get('wallet_xripple');
+    let xAddress = user.get("wallet_xripple");
 
     if (!xAddress && tag) {
       /**
@@ -503,5 +503,40 @@ export class AdminService {
     }
 
     return xAddress;
+  }
+
+  async fixPayLack(payrollID: string) {
+    const payroll_users = await db
+      .collection("payroll")
+      .doc(payrollID)
+      .collection("details")
+      .get();
+
+    const lack_to_pay = [];
+
+    for (const u of payroll_users.docs) {
+      const transactions = await db
+        .collection("payroll")
+        .doc(payrollID)
+        .collection("transactions")
+        .doc(u.get("id"))
+        .get();
+
+      const total_amount = await this.cryptoapisService.getXRPExchange(
+        u.get("total")
+      );
+      const lack = Number(total_amount) - Number(transactions.get("amount"));
+      const xAddress = await this.getXAddressUser(u.get("id"));
+
+      if (lack > 0) {
+        lack_to_pay.push({
+          amount: lack,
+          id_user: u.get("id"),
+          xAddress,
+        });
+      }
+    }
+
+    await this.payrollWithXRP(payrollID, lack_to_pay);
   }
 }
