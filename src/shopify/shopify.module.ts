f079d622bf359997f7aeb0a5c72d601193e7af80
victor alvarez <victoralvarezsaucedo@gmail.com>
2024-04-24 13:45:13 -0700

    fix api

--- src/shopify/shopify.module.ts
+++ src/shopify/shopify.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ShopifyService } from './shopify.service';
 import { ShopifyController } from './shopify.controller';
 import { BinaryService } from 'src/binary/binary.service';
+import { UsersService } from 'src/users/users.service';
 
 @Module({
-  providers: [ShopifyService],
-  controllers: [ShopifyController, BinaryService],
+  providers: [ShopifyService, BinaryService, UsersService],
+  controllers: [ShopifyController],
 })
 export class ShopifyModule {}
