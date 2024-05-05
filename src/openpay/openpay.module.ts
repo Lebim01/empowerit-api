90bed466519eba5431c0bcf7fbad939b94c582d5
Victor Alvarez <victoralvarezsaucedo@gmail.com>
2024-05-04 19:51:47 -0600

    fix nest

--- src/openpay/openpay.module.ts
+++ src/openpay/openpay.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { OpenpayController } from './openpay.controller';
 import { OpenpayService } from './openpay.service';
 import { SubscriptionsService } from 'src/subscriptions/subscriptions.service';
+import { BinaryService } from 'src/binary/binary.service';
+import { BondsService } from 'src/bonds/bonds.service';
+import { CryptoapisService } from 'src/cryptoapis/cryptoapis.service';
+import { GoogletaskService } from 'src/googletask/googletask.service';
+import { ShopifyService } from 'src/shopify/shopify.service';
+import { UsersService } from 'src/users/users.service';
 
 @Module({
   controllers: [OpenpayController],
-  providers: [OpenpayService, SubscriptionsService],
+  providers: [
+    OpenpayService,
+    SubscriptionsService,
+    BinaryService,
+    BondsService,
+    CryptoapisService,
+    GoogletaskService,
+    ShopifyService,
+    UsersService,
+  ],
 })
 export class OpenpayModule {}
