1c0e231d7c2992b8c5c94d4d5e9e4669f4442d4b
victor alvarez <victoralvarezsaucedo@gmail.com>
2024-05-10 20:30:06 -0600

    point

--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CartService } from './cart.service';
+import { db } from 'src/firebase/admin';
 
 @Controller('cart')
 export class CartController {
@@ -9,5 +10,14 @@ export class CartController {
   pay(@Body() body) {
     if (!body.id_user) throw new Error('User is required');
     return this.cartService.getPaymentLink(body.id_user);
+  }
+
+  @Post('fix')
+  async fix() {
+    const points = await db
+      .collectionGroup('right-points')
+      .where('user_id', '==', 'IOWwTlVPjSOmZAwTvsgFHoBShBE3')
+      .get();
+    return points.docs.map((r) => r.data());
   }
 }
