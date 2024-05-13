d777732f1fb92ad98450c384853cc92a631f77a0
victor alvarez <victoralvarezsaucedo@gmail.com>
2024-05-13 11:02:47 -0600

    status failed

--- src/openpay/openpay.service.ts
+++ src/openpay/openpay.service.ts
@@ -7,27 +7,45 @@ export class OpenpayService {
   constructor(private readonly subscriptionService: SubscriptionsService) {}
 
   async newChange(body: ChangeSuccess) {
-    if (body.type != 'charge.succeeded') return body;
+    if (body.type == 'payout.failed') {
+      const users = await db
+        .collection('users')
+        .where('email', '==', body.transaction.customer.email)
+        .get();
 
-    const users = await db
-      .collection('users')
-      .where('email', '==', body.transaction.customer.email)
-      .get();
+      if (!users.empty) {
+        const user = users.docs[0];
+        const payment_link = user.get('payment_link');
+        const membership = Object.keys(payment_link)[0] as Memberships;
 
-    if (!users.empty) {
-      const user = users.docs[0];
-      await user.ref.collection('openpay-transactions').add(body);
+        await user.ref.update({
+          [`payment_link.${membership}.status`]: 'failed',
+        });
 
-      const payment_link = user.get('payment_link');
-      const membership = Object.keys(payment_link)[0] as Memberships;
-      const period = payment_link[membership].membership_period || 'monthly';
-      await this.subscriptionService.onPaymentMembership(
-        user.id,
-        membership,
-        period,
-      );
+        return 'FAILED';
+      }
+    }
+    if (body.type == 'charge.succeeded') {
+      const users = await db
+        .collection('users')
+        .where('email', '==', body.transaction.customer.email)
+        .get();
 
-      return 'OK';
+      if (!users.empty) {
+        const user = users.docs[0];
+        await user.ref.collection('openpay-transactions').add(body);
+
+        const payment_link = user.get('payment_link');
+        const membership = Object.keys(payment_link)[0] as Memberships;
+        const period = payment_link[membership].membership_period || 'monthly';
+        await this.subscriptionService.onPaymentMembership(
+          user.id,
+          membership,
+          period,
+        );
+
+        return 'OK';
+      }
     }
 
     return 'FAIL';
