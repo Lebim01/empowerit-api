cc7f5bb4773ce7acb02fd5f97f466e6cec5a7e0a
Skared123 <raymundosanchez62@gmail.com>
2024-06-26 11:16:48 -0700

    funcion para hacer un update de los correos de mr algoritmo

--- src/algorithm-mr-range/algorithm-mr-range.service.ts
+++ src/algorithm-mr-range/algorithm-mr-range.service.ts
@@ -1,6 +1,7 @@
 import { HttpStatus, Injectable } from '@nestjs/common';
 import { collection, getDocs, query, where } from 'firebase/firestore';
 import { db } from 'src/firebase';
+import { db as admin } from '../firebase/admin';
 
 @Injectable()
 export class AlgorithmMrRangeService {
@@ -33,6 +34,31 @@ export class AlgorithmMrRangeService {
       return documentData;
     } catch (error) {
       console.log(error);
+    }
+  }
+
+
+  async updateEmail() {
+    try {
+      const licences = await admin
+      .collection('algorithm-license-history')
+      .get()
+
+      for (const doc of licences.docs){
+        let email = ''
+        if(doc.get('userId')){
+          const userRef = await admin
+          .collection('users')
+          .doc(doc.get('userId'))
+          .get()
+          const email = await userRef.get('email')
+          await doc.ref.update({
+            email
+          })
+        }
+      }
+    } catch (error) {
+      console.log(error)
     }
   }
 }
