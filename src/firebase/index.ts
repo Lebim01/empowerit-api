e3586e5b5104f8871c7caa428e5b899bf31d55b7
Skared123 <raymundosanchez62@gmail.com>
2024-06-28 13:34:12 -0700

    fix

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -4,13 +4,11 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
-const isProd = process.env.NODE_ENV == 'production'
-const databaseName = isProd ? '(default)' : 'testing'
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 // Initialize Cloud Firestore and get a reference to the service
-export const db = getFirestore(app,databaseName);
+export const db = getFirestore(app);
 
 export const auth = getAuth(app);
